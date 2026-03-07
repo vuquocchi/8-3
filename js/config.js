d5deb2571a39ca7f6ef6d55e68e3216557670ff0
vuquocchi <chivu2912k6@gmail.com>
2026-03-07 19:15:03 +0700

    init project2

--- js/config.js
+++ js/config.js
@@ -6,4 +6,4 @@ const base64 = "";
 const giftImageBase64 = "data:image/png;base64, " + base64;
 const titleCard = "8-3 ZUI ZẺ";
 const contentCard =
-  "Chúc đằng ấy ngày 8/3 nhiều nhiều niềm vui, nhiều nhiều năng lượng, xinh đẹp tuyệt vời nhớ! Mong rằng những điều tốt đẹp nhất sẽ đến với bạn trong hôm nay và tất nhiên là cả những ngày sau nữa;)";
+  "Chúc đằng ấy ngày 8/3 nhiều nhiều niềm vui, nhiều nhiều năng lượng, xinh đẹp tuyệt vời nhớ! Mong rằng những điều tốt đẹp nhất sẽ đến với đằng ấy trong hôm nay và tất nhiên là cả những ngày sau nữa;)";
